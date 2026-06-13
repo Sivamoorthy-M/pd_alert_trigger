37275c6cb1a99bb117e1c51794230d171ce7e864
Sivamoorthy_m <smoorthy@wsgc.com>
2026-06-13 19:06:12 +0530

    Commit add port

--- index.js
+++ index.js
@@ -73,6 +73,12 @@ app.get("/trigger/:tokenId", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`Listening on port ${PORT}`);
+// });
