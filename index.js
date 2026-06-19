53dea07798b9dbf0c30ab36beb083418db6d50b9
Sivamoorthy-M <sivamoorthy333@gmail.com>
2026-06-19 17:17:19 +0530

    Update index.js
    Email Field changed as a random.

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-app.get("/trigger/:tokenId", async (req, res) => {
+app.get("/trigger/:tokenId/:emailId", async (req, res) => {
     const tokenId = req.params.tokenId;
+    const emailId = req.params.emailId;
     try {
         // Get incidents
         const response = await axios.get(
@@ -37,7 +38,7 @@ app.get("/trigger/:tokenId", async (req, res) => {
                                 Authorization: `Token token=${tokenId}`,
                                 Accept: "application/vnd.pagerduty+json;version=2",
                                 "Content-Type": "application/json",
-                                From: "smoorthy@wsgc.com"
+                                From: `${emailId}`
                             }
                         }
                     );
